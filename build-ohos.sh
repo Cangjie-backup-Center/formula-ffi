122fb2b13c7f072737152aa9504cba65ecdc68eb
peixianzhong <peixianzhong@noreply.gitcode.com>
2025-09-15 08:46:12 +0800

    修改数学公式透明背景色问题

--- build-ohos.sh
+++ build-ohos.sh
@@ -5,7 +5,11 @@ mkdir -p lib
 cd ffi/build
 
 # -DCMAKE_CXX_COMPILER 和 -DCMAKE_CXX_FLAGS 路径自行设置
-cmake .. -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="D:/ohos/OHCangjie0.4.5-OH5.0.0.22/native/llvm/bin/clang++.exe" -DCMAKE_CXX_FLAGS="--sysroot=D:/ohos/OHCangjie0.4.5-OH5.0.0.22/native/sysroot --target=aarch64-linux-ohos" -DCMAKE_SYSTEM_NAME=linux -DCMAKE_SYSTEM_PROCESSOR=aarch64  -G "MinGW Makefiles" -DCMAKE_STRIP="D:/ohos/OHCangjie0.4.5-OH5.0.0.22/native/llvm/bin/llvm-strip.exe"
+
+cmake .. -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="D:/DevecoStudio_SDK/sdk/default/openharmony/native/llvm/bin/clang++.exe" -DCMAKE_CXX_FLAGS="--sysroot=D:/DevecoStudio_SDK/sdk/default/openharmony/native/sysroot --target=aarch64-linux-ohos" -DCMAKE_SYSTEM_NAME=linux -DCMAKE_SYSTEM_PROCESSOR=aarch64  -G "MinGW Makefiles" -DCMAKE_STRIP="D:/DevecoStudio_SDK/sdk/default/openharmony/native/llvm/bin/llvm-strip.exe"
+
+# cmake .. -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER="D:/DevecoStudio_SDK/sdk/default/openharmony/native/llvm/bin/clang++.exe" -DCMAKE_CXX_FLAGS="--sysroot=D:/DevecoStudio_SDK/sdk/default/openharmony/native/sysroot --target=x86_64-linux-ohos" -DCMAKE_SYSTEM_NAME=linux -DCMAKE_SYSTEM_PROCESSOR=x86_64  -G "MinGW Makefiles" -DCMAKE_STRIP="D:/DevecoStudio_SDK/sdk/default/openharmony/native/llvm/bin/llvm-strip.exe"
+
 make -j20
 
 cp liblatex.so ../../lib/
