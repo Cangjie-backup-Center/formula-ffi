97875193a6bd64e37c7d73e8ac65e49fca83fdc5
peixianzhong <peixianzhong@noreply.gitcode.com>
2026-01-08 17:20:30 +0800

    版本升级6.0.2

---
+++ formula_hybrid/src/main/cangjie/ark_interop_api_formula/Index.d.ts
@@ -0,0 +1,4 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025-2025. All rights reserved.
+ */
+export declare function latexStringToImageCJ(latexMathTextString: string, latexMathTextSize: number, latexMathTextColor: number, latexMathBackGroupColor: number, latexMathColorFormat: number, resPath: string): Promise<ArrayBuffer>
